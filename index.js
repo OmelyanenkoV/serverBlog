5a31cc680c4311d0e0c97271baa91a05a2a6bc2f
Vlad Omelianenko <vladislav.omelyanenko@p2h.com>
2021-10-03 19:05:22 +0300

    added routes, controllers, models

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const key = require('./keys')
+const authRoutes = require('./routes/auth.routes')
+
+const app = express()
+
+const PORT = 5000
+
+mongoose.connect(key.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((e) => console.error(e))
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// router
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => console.log(`The server listens on port ${PORT} .....`))
+
+
+
---
+++ keys/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: `mongodb+srv://vlad:q12345@cluster0.wdziv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
